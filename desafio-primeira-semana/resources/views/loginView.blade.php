b1739d82eefa603092cda98e0e5cb1e67f7c3f0c
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-05-06 17:49:10 -0300

    estilização do formulario

--- desafio-primeira-semana/resources/views/loginView.blade.php
+++ desafio-primeira-semana/resources/views/loginView.blade.php
@@ -2,15 +2,31 @@
 @section('content')
 
 
-            <div class="container">
-                    <form method='post' action="{{ route ('authenticate')}}">
-                        @csrf
-                        <b>login</b>
-                        <input type="text" name="email" required>
-                        <input type="password" name="password" required>
-                        <button type="submit" class="btn">Enviar</button>
-                </form>
-            </div>
-   
+<section>
+    <div class="background-section container-fluid">
+        <div class="container-login container">
+            <img src="{{ asset('img/eja-logo.png')}}" alt="..." style='width: 170px; height: 150px;'>
 
+                <form class="form-login" method='post' action="{{ route ('authenticate')}}">
+                        @csrf
+                        <p class="text-center">FAÇA SEU LOGIN</p>
+
+                        <div class="form-group">
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="LOGIN" required>
+                        </div>
+                        <div class="form-group">
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="SENHA" require>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary">Acesse agora</button>
+                </form>
+
+            <a href="#">Esqueci meu e-mail ou senha</a>
+
+        </div>
+    
+    </div>
+
+</section>
      @endsection
