2beec8865367355a65e434b90ca1c5bdf41018b4
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-05-07 10:25:49 -0300

    alteração do background

--- desafio-primeira-semana/resources/views/loginView.blade.php
+++ desafio-primeira-semana/resources/views/loginView.blade.php
@@ -1,32 +1,22 @@
 @extends('layout')
 @section('content')
-
-
-<section>
-    <div class="background-section container-fluid">
+<section class="background-section container-fluid">
+    <div class="container-fluid">
         <div class="container-login container">
             <img src="{{ asset('img/eja-logo.png')}}" alt="..." style='width: 170px; height: 150px;'>
-
                 <form class="form-login" method='post' action="{{ route ('authenticate')}}">
-                        @csrf
-                        <p class="text-center">FAÇA SEU LOGIN</p>
-
-                        <div class="form-group">
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="LOGIN" required>
-                        </div>
-                        <div class="form-group">
-                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="SENHA" require>
-                        </div>
-
-
-                        <button type="submit" class="btn btn-primary">Acesse agora</button>
+                    @csrf
+                    <p class="text-center">FAÇA SEU LOGIN</p>
+                    <div class="form-group">
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="LOGIN" required>
+                    </div>
+                    <div class="form-group">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="SENHA" require>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Acesse agora</button>
                 </form>
-
             <a href="#">Esqueci meu e-mail ou senha</a>
-
         </div>
-    
     </div>
-
 </section>
-     @endsection
+@endsection
