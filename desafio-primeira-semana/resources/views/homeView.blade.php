3bb66a9dce5e05ea6d2492e2b6a8c4c7019478e0
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-05-09 18:04:53 -0300

    estilização do card user

--- desafio-primeira-semana/resources/views/homeView.blade.php
+++ desafio-primeira-semana/resources/views/homeView.blade.php
@@ -10,12 +10,26 @@
     <x-sidebar/>
     <div class="home-right">
         <h1>lista de usuarios: +++</h1>
-        @foreach($users as $user)
-            <p>nome {{$user->name}}</p>
-        @endforeach
-     
+        <div class="card card-user">
+             <div class="card-user-content">
+                <div class="card-user-left">
+                    <div class="card-user-img">
+                        <img src="{{asset ('img/user-white.svg')}}" alt="">
+                    </div>
+                    <div class="card-user-data">
+                        <div class="card-user-data-nome"> jorel</div>
+                        <div class="card-user-data-email">jorel@gmail.com</div>
+                    </div>
+                </div>
+                <div class="card-user-right">
+                    <button class="btn-edit-user"><img src="{{asset('img/trash.svg')}}" alt=""></button>
+                    <button class="btn-delete-user"><img src="{{asset('img/note-pencil.svg')}}" alt=""></button>
+                </div>
+            </div>
+        </div>
+
     </div>
-    </div>
+</div>
 
 </section>
 @endsection
