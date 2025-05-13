03def93fe26251793b5b8315154dab4d91f87295
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-05-13 15:09:07 -0300

    update

--- desafio-primeira-semana/resources/views/components/sidebar.blade.php
+++ desafio-primeira-semana/resources/views/components/sidebar.blade.php
@@ -1,6 +1,5 @@
 <div class="home-left">
         <img class="edus-logo-img" src="{{asset ('img/edus-logo.svg')}}" alt="">
-
         <nav class="nav-items">
             <button class="button-nav-left" onclick="window.location='{{ route('home') }}'">
                 <img src="{{asset ('img/house.svg')}}" alt="">
