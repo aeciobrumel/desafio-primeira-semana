74f72fd058c0b70238372fa46234e87f09240c0c
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-05-20 15:01:05 -0300

    upload de imagens

--- desafio-primeira-semana/resources/views/layout.blade.php
+++ desafio-primeira-semana/resources/views/layout.blade.php
@@ -35,40 +35,40 @@
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script>
-    //Script dos modais de sucesso e erro no login
-    window.onload = function(){
-            @if(session('login-error'))
-                var failedModal = new bootstrap.Modal(document.getElementById('login-error-modal'));
-                failedModal.show();
-            @endif
+      //Script dos modais de sucesso e erro no login
+      window.onload = function(){
+              @if(session('login-error'))
+                  var failedModal = new bootstrap.Modal(document.getElementById('login-error-modal'));
+                  failedModal.show();
+              @endif
 
-            @if(session('login-success'))
-                var sucessModal = new bootstrap.Modal(document.getElementById('login-success-modal'));
-                sucessModal.show();
-            @endif
+              @if(session('login-success'))
+                  var sucessModal = new bootstrap.Modal(document.getElementById('login-success-modal'));
+                  sucessModal.show();
+              @endif
+          }
+
+      //formata cpf
+      function formatCPF(input) {
+        let value = input.value.replace(/\D/g, ""); // Remove caracteres não numéricos
+        value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o primeiro ponto
+        value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o segundo ponto
+        value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // Adiciona o traço
+        input.value = value;
         }
+        
+      //tira a mensagem de erro global
+      setTimeout(() => {
+      const alert = document.getElementById('floating-alert');
+          if (alert) {
+              alert.classList.remove('show'); // fade do Bootstrap
+              setTimeout(() => alert.remove(), 500); // remove após fade
+          }
+        }, 3000); // 5 segundos
 
-//formata cpf
-    function formatCPF(input) {
-      let value = input.value.replace(/\D/g, ""); // Remove caracteres não numéricos
-      value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o primeiro ponto
-      value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o segundo ponto
-      value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // Adiciona o traço
-      input.value = value;
-      }
-      
-    //tira a mensagem de erro global
-    setTimeout(() => {
-    const alert = document.getElementById('floating-alert');
-        if (alert) {
-            alert.classList.remove('show'); // fade do Bootstrap
-            setTimeout(() => alert.remove(), 500); // remove após fade
-        }
-      }, 3000); // 5 segundos
-
-          //Script para tela de login
-          //escutador de evento para o submit do formulário
-          // adiciona um escutador de evento para o evento 'DOMContentLoaded'
+            //Script para tela de login
+            //escutador de evento para o submit do formulário
+            // adiciona um escutador de evento para o evento 'DOMContentLoaded'
       document.addEventListener('DOMContentLoaded', function () {
           const form = document.getElementById('login-form');
           // Adiciona um escutador de evento para captar o evento 'submit' do formulário
@@ -81,35 +81,51 @@
                 form.submit();
             }, 500); // 500 milissegundos = 0,5 segundos
           });
-      });
-    //script logout sidebar
+        });
+      //script logout sidebar
       function logoutPost() {
-            const formLogout = document.createElement('form');
-            formLogout.method = 'POST';
-            formLogout.action ='{{route('logout')}}';
+              const formLogout = document.createElement('form');
+              formLogout.method = 'POST';
+              formLogout.action ='{{route('logout')}}';
 
-            const csrf = document.createElement('input');
-            csrf.type = 'hidden';
-            csrf.name= '_token';
-            csrf.value = '{{csrf_token()}}';
+              const csrf = document.createElement('input');
+              csrf.type = 'hidden';
+              csrf.name= '_token';
+              csrf.value = '{{csrf_token()}}';
 
-            formLogout .appendChild(csrf);
-            document.body.appendChild(formLogout);
-            formLogout.submit();
-          }
-    // botão de rota pro form
+              formLogout .appendChild(csrf);
+              document.body.appendChild(formLogout);
+              formLogout.submit();
+        }
+      // botão de rota pro form
       function goTocreateUserForm(){
-        const formRouteCreateUser = document.createElement('form');
-        formRouteCreateUser.method = 'get';
-        formRouteCreateUser.action ='{{route('users.create')}}';
-        const csrf = document.createElement('input');
-        csrf.type = 'hidden';
-        csrf.name= '_token';
-        csrf.value = '{{csrf_token()}}';
-        formRouteCreateUser .appendChild(csrf);
-        document.body.appendChild(formRouteCreateUser);
-        formRouteCreateUser.submit();
-      }    
+          const formRouteCreateUser = document.createElement('form');
+          formRouteCreateUser.method = 'get';
+          formRouteCreateUser.action ='{{route('users.create')}}';
+          const csrf = document.createElement('input');
+          csrf.type = 'hidden';
+          csrf.name= '_token';
+          csrf.value = '{{csrf_token()}}';
+          formRouteCreateUser .appendChild(csrf);
+          document.body.appendChild(formRouteCreateUser);
+          formRouteCreateUser.submit();
+        }    
+
+      //preview img       
+      document.getElementById('photo').addEventListener('change', function (e) {
+          const file = e.target.files[0];
+          const preview = document.getElementById('avatarPreview');
+
+            if (file && file.type.startsWith('image/')) {
+                const reader = new FileReader();
+
+                reader.onload = function (event) {
+                    preview.src = event.target.result;
+                };
+
+                reader.readAsDataURL(file);
+            }
+        }); 
   </script>
 </body>
 </html>
--- desafio-primeira-semana/resources/views/users/partials/form.blade.php
+++ desafio-primeira-semana/resources/views/users/partials/form.blade.php
@@ -1,48 +1,65 @@
 
 <form class="form-cadastro" id="form-cadastro" method='post' action="{{isset($user) ? route('users.update', $user->id) : route('users.store')}}" enctype="multipart/form-data" >
-    @csrf
+   @csrf
     @if(isset($user))
         @method('PUT')
     @endif
-    <div class="input-group mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-default">Nome:</span>
-        <input name="name" value="{{ old('name', $user->name ?? '' )}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-    </div>
-    <div class="input-group mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-default">Email:</span>
-        <input name="email" value="{{old('email', $user->email ?? ' ') }}" type="email" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-    </div>
-    <div class="input-group mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-default">cpf:</span>
-        <input id="cpf" oninput="formatCPF(this)" maxlength=14 placeholder="000.000.000-00" name="cpf" value="{{old('cpf', isset($user) ? $user->getCpfFormattedAttribute() : '') }}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"  required>
-    </div>
-    <div class="row">
-        <div class="input-group mb-3 col">
-            <span class="input-group-text" id="inputGroup-sizing-default">Senha:</span>
-            <input name="password" type="password" class="form-control">              
+   <div  class="form-cadastro-inputs">
+        <div class="input-group mb-3 form-border-content">
+            <span class="input-group-text" id="inputGroup-sizing-default">Nome:</span>
+            <input name="name" value="{{ old('name', $user->name ?? '' )}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="input-group mb-3 col">
-            <span class="input-group-text" id="inputGroup-sizing-default">confirmar senha:</span>
-            <input name="confirm_password" type="password" class="form-control">               
+        <div class="input-group mb-3 form-border-content">
+            <span class="input-group-text" id="inputGroup-sizing-default">Email:</span>
+            <input name="email" value="{{old('email', $user->email ?? ' ') }}" type="email" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
-        <div class="input-group mb-3 col">
-            <label class="input-group-text" for="inputGroupSelect01">Permissão:</label>            
-            <select name="permission" class="form-select" id="inputGroupSelect01">
-                    <option value="" disabled {{ old('permission', isset($user) ? $user->permission_level : '') == '' ? 'selected' : '' }}>
-                    Selecione...
-                    </option>
-                    @foreach($permissions as $permission)
-                    <option value="{{$permission->value}}" {{old('permission', isset($user) ? $user->permission_level : '')==$permission->value ? 'selected' : '' }}>
-                        {{ $permission -> name }}
-                    </option>
-                    @endforeach
-            </select>
+        
+        <div class="row form-border-content">
+            <div class="input-group mb-3 col">
+                <span class="input-group-text" id="inputGroup-sizing-default">cpf:</span>
+                <input id="cpf" oninput="formatCPF(this)" maxlength=14 placeholder="000.000.000-00" name="cpf" value="{{old('cpf', isset($user) ? $user->getCpfFormattedAttribute() : '') }}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"  required>
+            </div>
+            <div class="input-group mb-3 col ">
+                <label class="input-group-text " for="inputGroupSelect01">Permissão:</label>            
+                <select name="permission" class="form-select" id="inputGroupSelect01">
+                        <option value="" disabled {{ old('permission', isset($user) ? $user->permission_level : '') == '' ? 'selected' : '' }}>
+                        Selecione...
+                        </option>
+                        @foreach($permissions as $permission)
+                        <option value="{{$permission->value}}" {{old('permission', isset($user) ? $user->permission_level : '')==$permission->value ? 'selected' : '' }}>
+                            {{ $permission -> name }}
+                        </option>
+                        @endforeach
+                </select>
+            </div>
         </div>
-    </div>
-    <div class="input-group mb-3 col">
-        <span class="input-group-text" id="inputGroup-sizing-default">Foto:</span>
-        <input name="photo" type="file" class="form-control" accept="image/*">
-    </div>
+        <div class="row form-border-content">
+            <div class="input-group mb-3 col">
+                <span class="input-group-text" id="inputGroup-sizing-default">Senha:</span>
+                <input name="password" type="password" class="form-control">              
+            </div>
+            <div class="input-group mb-3 col">
+                <span class="input-group-text" id="inputGroup-sizing-default">confirmar senha:</span>
+                <input name="confirm_password" type="password" class="form-control">               
+            </div>
+            
+        </div>
+        <div class="avatar-container" >
+            <div class="avatar-left">
+                <span class="title avatar">Foto de avatar</span>
+                <div class="input-group mb-3 avatar-input col">
+                    <input name="photo" id="photo" type="file" class="form-control" accept="image/*">
+                </div>
+            </div>
+            <div class="avatar-right">
+                <div class="form-user-avatar ">
+                    <img id="avatarPreview"  src="{{ isset($user) ? $user->photo_url : asset('img/user-white.svg') }}" alt="Foto do usuário">
+                </div>
+            </div>
+        </div>
+   </div>
+    
+    
     <div class="container-btn">
         <button type="submit" class="btn btn-primary btn-enviar-usuario">
         {{ isset($user) ? 'Editar usuário' : 'Cadastrar usuário'}}
